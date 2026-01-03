96f8a98c1dc31fcff795f1a95d9a908eb5749d58
Niicka <lnk1101603@kau.kr>
2026-01-04 02:59:06 +0900

    initial: springboot 프로젝트 초기 설정

---
+++ src/main/java/com/symteo/SymteoApplication.java
@@ -0,0 +1,13 @@
+package com.symteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SymteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SymteoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/symteo/SymteoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.symteo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SymteoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
