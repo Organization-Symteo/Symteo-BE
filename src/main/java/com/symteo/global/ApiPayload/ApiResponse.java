50773bd1f260499fdc2315443196009e90258b9c
Niicka <lnk1101603@kau.kr>
2026-01-08 18:23:37 +0900

    feat: Swagger API 명세서 도입 및 보안 설정 허용 (#5)

--- src/main/java/com/symteo/global/ApiPayload/ApiResponse.java
+++ src/main/java/com/symteo/global/ApiPayload/ApiResponse.java
@@ -7,7 +7,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
 @AllArgsConstructor
 @JsonPropertyOrder({"isSuccess", "code", "message", "result"})
 public class ApiResponse<T> {
