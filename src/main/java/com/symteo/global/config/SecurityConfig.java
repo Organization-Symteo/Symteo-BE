7d4768b8ff8dad9658334f358fd1a52689f04aba
besttomato <spacejin2000@naver.com>
2026-02-06 14:30:35 +0000

    feat: grafana, prometheus 설정 도입, AiReport 불러오기 기능 완료, 진단 로직 수정

--- src/main/java/com/symteo/global/config/SecurityConfig.java
+++ src/main/java/com/symteo/global/config/SecurityConfig.java
@@ -37,7 +37,7 @@ public class SecurityConfig {
                         // (1) Swagger 및 인증 관련 API는 모두 허용
                         .requestMatchers(
                                 "/swagger-ui/**", "/v3/api-docs/**", "/swagger-ui.html",
-                                "/api/v1/auth/**", "/error", "/favicon.ico"
+                                "/api/v1/auth/**", "/error", "/favicon.ico", "/actuator/health", "/actuator/prometheus"
                         ).permitAll()
 
                         // DevAuthController 관련 내용이므로 이후 삭제 에정(개발용 로그인 경로는 프리패스 허용)
