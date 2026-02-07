782b3e71365164f49f074d3c308334faa4c58e2b
YewonHwangeayo <141620327+YewonHwangeayo@users.noreply.github.com>
2026-02-07 18:58:31 +0900

    fix: 회원가입 로직 수정2

--- src/main/java/com/symteo/global/auth/dto/LoginRequest.java
+++ src/main/java/com/symteo/global/auth/dto/LoginRequest.java
@@ -7,6 +7,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 //소셜 로그인을 통해 발급받은 access token을 앱 토큰(JWT)로 변경
 public class LoginRequest {
-    private String authCode; // 프론트에서 주는 소셜 Access Token
+    private String authCode; // 프론트에서 주는 authCode
 
 }
