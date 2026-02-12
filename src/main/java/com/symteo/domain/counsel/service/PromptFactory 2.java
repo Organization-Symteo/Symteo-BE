264b1b3e52e5b8cabb6ef9b778a041a5048c25c9
Lee Na gyeong <163131079+Niicka@users.noreply.github.com>
2026-02-12 23:38:06 +0900

    Revert "Niicka/34/missions"

--- src/main/java/com/symteo/domain/counsel/service/PromptFactory 2.java
+++
@@ -1,7 +0,0 @@
-package com.symteo.domain.counsel.service;
-
-public interface PromptFactory {
-    public String createSBPrompt(Long userId, Long reportId);
-    public String createDAPrompt(Long userId, Long reportId);
-    public String createAPrompt(Long userId, Long reportId);
-}
--- src/main/java/com/symteo/domain/user/dto/UpdateMissionRequest 2.java
+++
@@ -1,9 +0,0 @@
-package com.symteo.domain.user.dto;
-
-import lombok.Getter;
-
-@Getter
-public class UpdateMissionRequest {
-    private String contents; // 수정할 일기 내용
-}
-
